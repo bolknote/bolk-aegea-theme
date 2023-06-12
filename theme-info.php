89e1d1f0d2e0c870e034e9b5bc9678ef810f2e1f
Evgeny Stepanischev <imbolk@gmail.com>
2023-06-12 21:41:50 +0300

    Initial commit

---
+++ theme-info.php
@@ -0,0 +1,23 @@
+<?php return [
+
+  'index' => 0,
+
+  'display_name' => [
+    'en' => 'Bolk',
+    'ru' => 'Болк',
+    'uk' => 'Бобк',
+  ],
+
+  'colors' => [
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#0060a0',
+  ],
+
+  'based_on' => 'plain',
+
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => true,
+
+];
